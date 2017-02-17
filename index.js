48ec9975cf7dec42e0b18b9376c0f8cdbe73289b
JiangHuan <278362241@qq.com>
2017-02-17 15:03:46 +0800

    update

--- index.js
+++ index.js
@@ -32,10 +32,13 @@ app.post('/postdata', function(req, res){
     });
     var insertSQL = "INSERT INTO  `app_demorecommend`.`form` (`name`, `phone`, `email`, `school`, `interestGroupId`, `jobId`) VALUES ('" + data.name + "', '" + data.phone + "', '" + data.email + "', '" + data.school + "', '" + data.interestGroupId + "', '" + data.jobId + "')";
 
-
     connection.connect();
 
     connection.query(insertSQL, function (err1, res1) {
+        console.log(process.env);
+        console.log(err1);
+        console.log(res1);
+        console.log(insertSQL);
         if (err1) {
             res.send("信息上传失败，请重新填写信息");
             return;
@@ -43,7 +46,7 @@ app.post('/postdata', function(req, res){
     })
 
     connection.end();
-    console.log(process.env);
+    
     //res.redirect("/success");
 });
 
