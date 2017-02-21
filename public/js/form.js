20bb00032cbe07133404fdbc2e079033cdfb3de4
alivebao <641572831@qq.com>
2017-02-21 23:40:11 +0800

    update reiminder style

--- public/js/form.js
+++ public/js/form.js
@@ -1,34 +1,30 @@
-
-$(function() {
+$(function () {
     var requestSending = false;  //用户是否点击提交按钮
 
-    $($('#form1 .radioGourp input')[0]).on('click', function(){
+    $($('#form1 .radioGourp input')[0]).on('click', function () {
         //is regular
         $('.only-intern').addClass('hide');
         $('.only-regular').removeClass('hide');
     });
-    $($('#form1 .radioGourp input')[1]).on('click', function(){
+    $($('#form1 .radioGourp input')[1]).on('click', function () {
         //is intern
         $('.only-regular').addClass('hide');
         $('.only-intern').removeClass('hide');
     });
-    $('.J_submit').on('click', function() {
-        $('#form1').click(function(){
-            validateForm();
-        });
-        if(!validateForm()) {
+    $('.J_submit').on('click', function () {
+        if (!validateForm()) {
             return;
         }
         $(".J_submit").addClass("grey").text('提交中');
-        if(requestSending){
-            return ;
+        if (requestSending) {
+            return;
         }
-        var $selectedOption = $('.J_Job').find("option").not(function(){ return !this.selected }),
+        var $selectedOption = $('.J_Job').find("option").not(function () { return !this.selected }),
             jobId = 0;
-       
+
         if ($('.J_regular').prop('checked')) {
             jobId = $selectedOption.data('regularid');
-        }else {
+        } else {
             jobId = $selectedOption.data('internid');
         }
         var postdata = {
@@ -36,8 +32,8 @@ $(function() {
             phone: $('.J_phone').val(),
             email: $('.J_email').val(),
             school: $('.J_school').val() || '',
-            interestGroupId:  $('.J_Group').val() || '1',
-            jobId:  jobId || '7074',
+            interestGroupId: $('.J_Group').val() || '1',
+            jobId: jobId || '7074',
         };
         requestSending = true;
         $.ajax({
@@ -47,16 +43,16 @@ $(function() {
             success: function (data) {
                 requestSending = false;
                 data = data || {};
-                
-                if(data.statusCode === 1){
-                    var showSuccess = function(){
+
+                if (data.statusCode === 1) {
+                    var showSuccess = function () {
                         $('.recom-success').removeClass("hide");
                     }
-                    Toast('上传成功', showSuccess, 2000, '', true);                    
+                    Toast('上传成功', showSuccess, 2000, '', true);
                 }
-                else if(data.statusCode === -1){
+                else if (data.statusCode === -1) {
                     Toast('该手机号已注册', '', 2000, '', true);
-                }else if(data.statusCode === -2){
+                } else if (data.statusCode === -2) {
                     Toast('系统错误，请稍后尝试', '', 2000, '', true);
                 }
                 $(".J_submit").removeClass("grey").text('提交');
@@ -68,15 +64,7 @@ $(function() {
                 $(".J_submit").removeClass("grey").text('提交');
             }
         });
-    }) 
-})
-
-function validateForm() {
-    var input_name = $('.J_name').val().trim(),
-        input_phone = $('.J_phone').val().trim(),
-        input_email = $('.J_email').val().trim(),
-        input_school = $('.J_school').val().trim(),
-        input_job = $('.J_Job').val().trim();
+    })
 
     var phoneRegx = /^1\d{10}$/,
         emailRegx = /^[a-zA-Z.0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
@@ -90,43 +78,111 @@ function validateForm() {
         err_empty_mail = '邮箱地址不可为空!',
         err_empty_school = '学校不可为空!';
 
-    if(!input_name){
-        displayError(err_empty_name);
-        return false;
+    $('#form1 .J_Job').click(function () {
+        if ($('.J_Job .groupDefault').val() !== $(this).val() && $('.J_error').text() == err_job) {
+            $('.J_error').addClass('hide');
+        }
+    });
+
+    $('#form1 .inputDiv input').blur(function () {
+        var input_name = $('.J_name').val().trim(),
+            input_phone = $('.J_phone').val().trim(),
+            input_email = $('.J_email').val().trim(),
+            input_school = $('.J_school').val().trim(),
+            input_job = $('.J_Job').val().trim();
+
+        if ($(this).attr("class") === "J_name") {
+            if (!input_name) {
+                displayError(err_empty_name);
+            } else if($('.J_error').text() == err_empty_name){
+                $('.J_error').addClass('hide');
+            }
+        }
+
+        if ($(this).attr("class") === "J_phone") {
+            if (!input_phone) {
+                displayError(err_empty_phone);
+            } else if (!phoneRegx.test(input_phone)) {
+                displayError(err_phone);
+            } else if($('.J_error').text() == err_empty_phone || $('.J_error').text() == err_phone){
+                $('.J_error').addClass('hide');
+            }
+        }
+
+        if ($(this).attr("class") === "J_email") {
+            if (!input_email) {
+                displayError(err_empty_mail);
+            } else if (!emailRegx.test(input_email)) {
+                displayError(err_mail);
+            } else if($('.J_error').text() == err_empty_mail || $('.J_error').text() == err_mail){
+                $('.J_error').addClass('hide');
+            }
+        }
+
+        if ($(this).attr("class") === "J_school") {
+            if (!input_school) {
+                displayError(err_empty_school);
+            } else if($('.J_error').text() == err_empty_school){
+                $('.J_error').addClass('hide');
+            }
+        }
+
+        if ($(this).attr("class") === "J_Job") {
+            if ($('.J_Job .groupDefault').val() === input_job) {
+                displayError(err_empty_name);
+            } else if($('.J_error').text() == err_empty_name){
+                $('.J_error').addClass('hide');
+            }
+        }
+    });
+
+    function validateForm() {
+        var input_name = $('.J_name').val().trim(),
+            input_phone = $('.J_phone').val().trim(),
+            input_email = $('.J_email').val().trim(),
+            input_school = $('.J_school').val().trim(),
+            input_job = $('.J_Job').val().trim();
+
+        if (!input_name) {
+            displayError(err_empty_name);
+            return false;
+        }
+
+        if (!input_phone) {
+            displayError(err_empty_phone);
+            return false;
+        }
+
+        if (!input_email) {
+            displayError(err_empty_mail);
+            return false;
+        }
+
+        if (!input_school) {
+            displayError(err_empty_school);
+            return false;
+        }
+
+        if (!phoneRegx.test(input_phone)) {
+            displayError(err_phone);
+            return false;
+        }
+
+        if (!emailRegx.test(input_email)) {
+            displayError(err_mail);
+            return false;
+        }
+        if ($('.J_Job .groupDefault').val() === input_job) {
+            displayError(err_job);
+            return false;
+        }
+
+        return true;
     }
 
-    if(!input_phone){
-        displayError(err_empty_phone);
-        return false;
+    function displayError(err_msg) {
+        $('.J_error').text(err_msg).removeClass('hide')
     }
+})
 
-    if(!input_email){
-        displayError(err_empty_mail);
-        return false;
-    }
 
-    if(!input_school){
-        displayError(err_empty_school);
-        return false;
-    }
-
-    if (!phoneRegx.test(input_phone)) {
-        displayError(err_phone);
-        return false;
-    }
-
-    if (!emailRegx.test(input_email)) {
-        displayError(err_mail);
-        return false;
-    }
-    if($('.J_Job .groupDefault').val() === input_job){
-        displayError(err_job);
-        return false;
-    }
-    $('.J_error').addClass('hide')
-    return true;
-}
-
-function displayError(err_msg) {
-    $('.J_error').text(err_msg).removeClass('hide')
-}
